4888d4b9ee19a36aac96506ece8bd19cf9232f41
Ian <ianproth@gmail.com>
2015-08-19 16:13:41 -0700

    Reformat drive class for readability

--- src/drive.py
+++ src/drive.py
@@ -3,19 +3,14 @@ __author__ = 'Ian'
 import math
 import wpilib
 
-from wpilib.motorsafety import MotorSafety
+# from wpilib.motorsafety import MotorSafety #Implement this in the future
+
 from enum import Enum
 
 __all__ = ["Drive"]
 
 
-
-
-# put something in here free motors bla bla
-
-
 class Drive( ):
-
 	PI_DIV_4 = math.pi / 4.0
 
 	class MotorPosition( Enum ):
@@ -31,16 +26,16 @@ class Drive( ):
 
 			self.setPoint = 0.0
 
-		def motorI(self):
+		def motorI( self ):
 			return -1 if self.motorInverted else 1
 
 	kDefaultExpirationTime = 0.1
 	kDefaultSensitivity = 0.5
 	kDefaultMaxOutput = 1.0
 
-	acceptedFilterTypes = ["1_1","2_2"]
+	acceptedFilterTypes = ["1_1", "2_2"]
 
-	def __init__( self, FL , FR , RL  , RR  , config):
+	def __init__( self, FL, FR, RL, RR, config ):
 
 		super( ).__init__( )
 
@@ -49,7 +44,6 @@ class Drive( ):
 		self.RLMotor = RL
 		self.RRMotor = RR
 		self.config = config
-
 
 		self.enabled = False
 
@@ -72,7 +66,7 @@ class Drive( ):
 		self.RFilter = None
 		self.MDFilter = None
 
-	#Set Functions
+	# Set Functions
 	def setTalonConfig( self, CANTalonConfig ):
 		self.config = CANTalonConfig
 
@@ -101,9 +95,10 @@ class Drive( ):
 		self.prescaleR = Rotation
 		self.prescaleT = Translation
 
-	def setSinInverted(self, inverted):
+	def setSinInverted( self, inverted ):
 		self.sinInverted = inverted
-	#Enable And Disable
+
+	# Enable And Disable
 	def enable( self ):
 		self.enabled = True
 		self.configMotors( )
@@ -119,7 +114,8 @@ class Drive( ):
 		self.RLMotor.set( 0.0 )
 		self.MInfoRR.setPoint = 0.0
 		self.RRMotor.set( 0.0 )
-	#Drive Functions
+
+	# Drive Functions
 	def setTranslation( self, X, Y ):
 		self.TX = X * math.sqrt( 2 ) * self.prescaleT
 		self.TY = Y * self.prescaleT
@@ -131,30 +127,32 @@ class Drive( ):
 		LX = self.TX
 		LY = self.TY
 		LR = self.TR
-		# implement xy filters here?
+
+		# XY filters
 		if self.XYFilter != None:
 			for filter in self.XYFilter:
-				filter.compute(LX , LY)
-				LX = filter.readA()
-				LY = filter.readB()
-		# implement rotation filters here
+				filter.compute( LX, LY )
+				LX = filter.readA( )
+				LY = filter.readB( )
+				
+		# Rotation filters
 		if self.RFilter != None:
 			for filter in self.RFilter:
-				filter.compute(LR)
-				LR = filter.readA()
+				filter.compute( LR )
+				LR = filter.readA( )
 
 		forceMag = math.sqrt( (LX * LX) + (LY * LY) )
 
 		forceAngle = math.atan2( LX, LY )
 
-		forceAngle += self.PI_DIV_4
+		forceAngle += Drive.PI_DIV_4
 
-		# implement mag direction filter
+		# Magnitude / Direction filters
 		if self.MDFilter != None:
 			for filter in self.MDFilter:
-				filter.compute(forceMag , forceAngle)
-				forceMag = filter.readA()
-				forceAngle = filter.readB()
+				filter.compute( forceMag, forceAngle )
+				forceMag = filter.readA( )
+				forceAngle = filter.readB( )
 
 		sinCalc = math.sin( forceAngle ) * forceMag
 		cosCalc = math.cos( forceAngle ) * forceMag
@@ -162,13 +160,12 @@ class Drive( ):
 		if self.enabled:
 			Speeds = [0] * 4
 
-			Speeds[0] = ((cosCalc if self.sinInverted else sinCalc) + LR) * self.MInfoFL.motorI()
-			Speeds[1] = ((sinCalc if self.sinInverted else cosCalc) - LR) * self.MInfoFR.motorI()
-			Speeds[2] = ((sinCalc if self.sinInverted else cosCalc) + LR) * self.MInfoRL.motorI()
-			Speeds[3] = ((cosCalc if self.sinInverted else sinCalc) - LR) * self.MInfoRR.motorI()
+			Speeds[0] = ((cosCalc if self.sinInverted else sinCalc) + LR) * self.MInfoFL.motorI( )
+			Speeds[1] = ((sinCalc if self.sinInverted else cosCalc) - LR) * self.MInfoFR.motorI( )
+			Speeds[2] = ((sinCalc if self.sinInverted else cosCalc) + LR) * self.MInfoRL.motorI( )
+			Speeds[3] = ((cosCalc if self.sinInverted else sinCalc) - LR) * self.MInfoRR.motorI( )
 
 			wpilib.RobotDrive.normalize( Speeds )
-
 
 			self.scaleSpeeds( Speeds )
 
@@ -180,13 +177,13 @@ class Drive( ):
 			self.RLMotor.set( Speeds[2] )
 			self.MInfoRR.setPoint = Speeds[3]
 			self.RRMotor.set( Speeds[3] )
-	#Get Functions
+
+	# Get Functions
 	def getEnabled( self ):
 		return self.enabled
 
 	def getMotorScale( self ):
 		return self.MaxV
-
 
 	def getPreScaleT( self ):
 		return self.prescaleT
@@ -200,7 +197,7 @@ class Drive( ):
 	def getNumMotors( self ):
 		return 4
 
-	#Utility Functions
+	# Utility Functions
 	def stopMotor( self ):
 		if self.FLMotor is not None:
 			self.FLMotor.set( 0.0 )
@@ -210,7 +207,6 @@ class Drive( ):
 			self.RLMotor.set( 0.0 )
 		if self.RRMotor is not None:
 			self.RRMotor.set( 0.0 )
-		#self.feed( )
 
 	def scaleSpeeds( self, WheelSpeeds ):
 		for i in range( len( WheelSpeeds ) ):
@@ -230,62 +226,60 @@ class Drive( ):
 
 	def enabledMotorFix( self ):
 		if self.enabled:
+			self.configSensorI( )
 
-			self.configSensorI()
+			self.FLMotor.set( self.MInfoFL.setPoint * (-1 if self.MInfoFL.motorInverted else 1) )
+			self.FRMotor.set( self.MInfoFR.setPoint * (-1 if self.MInfoFR.motorInverted else 1) )
+			self.RLMotor.set( self.MInfoRL.setPoint * (-1 if self.MInfoRL.motorInverted else 1) )
+			self.RRMotor.set( self.MInfoRR.setPoint * (-1 if self.MInfoRR.motorInverted else 1) )
 
-			self.FLMotor.set(
-				self.MInfoFL.setPoint * (-1 if self.MInfoFL.motorInverted else 1) )
-			self.FRMotor.set(
-				self.MInfoFR.setPoint * (-1 if self.MInfoFR.motorInverted else 1) )
-			self.RLMotor.set(
-				self.MInfoRL.setPoint * (-1 if self.MInfoRL.motorInverted else 1) )
-			self.RRMotor.set(
-				self.MInfoRR.setPoint * (-1 if self.MInfoRR.motorInverted else 1) )
-
-	def checkFilterType(self, filter):
+	def checkFilterType( self, filter ):
 		if hasattr( filter, "getType" ):
-			if filter.getType() not in self.acceptedFilterTypes:
-				raise ValueError("That is not a valid type of filter")
-			return filter.getType()
+			if filter.getType( ) not in self.acceptedFilterTypes:
+				raise ValueError( "That is not a valid type of filter fo this application" )
+			return filter.getType( )
 		else:
-			raise ValueError("did not pass in a valid filter")
+			raise ValueError( "did not pass in a valid filter" )
 
-	def addXYFilter(self, filter):
-		if filter != None and self.checkFilterType(filter) == "2_2":
-			if  self.XYFilter == None:
+	def addXYFilter( self, filter ):
+		if filter != None and self.checkFilterType( filter ) == "2_2":
+			if self.XYFilter == None:
 				self.XYFilter = []
-			self.XYFilter.append(filter)
-	def removeXYFilter(self, filter):
-		#note this only gets the first index of that filter, same for all of the remove functions
-		if filter != None and self.checkFilterType(filter) == "2_2":
-			ind = self.XYFilter.index(filter)
+			self.XYFilter.append( filter )
+
+	def removeXYFilter( self, filter ):
+		# note this only gets the first index of that filter, same for all of the remove functions
+		if filter != None and self.checkFilterType( filter ) == "2_2":
+			ind = self.XYFilter.index( filter )
 			if ind != -1:
-				self.XYFilter.pop(ind)
-				if len(self.XYFilter) == 0:#serperate this out into its own function at some point..
+				self.XYFilter.pop( ind )
+				if len( self.XYFilter ) == 0:  # serperate this out into its own function at some point..
 					self.XYFilter = None
 
-	def addRFilter(self, filter):
-		if filter != None and self.checkFilterType(filter) == "1_1":
+	def addRFilter( self, filter ):
+		if filter != None and self.checkFilterType( filter ) == "1_1":
 			if self.RFilter == None:
 				self.RFilter = []
-			self.RFilter.append(filter)
-	def removeRFilter(self, filter):
-		if filter != None and self.checkFilterType(filter) == "1_1":
-			ind = self.RFilter.index(filter)
+			self.RFilter.append( filter )
+
+	def removeRFilter( self, filter ):
+		if filter != None and self.checkFilterType( filter ) == "1_1":
+			ind = self.RFilter.index( filter )
 			if ind != -1:
-				self.RFilter.pop(ind)
-				if len(self.RFilter) == 0:
+				self.RFilter.pop( ind )
+				if len( self.RFilter ) == 0:
 					self.RFilter = None
 
-	def addMDFilter(self,filter):
-		if filter != None and self.checkFilterType(filter) == "2_2":
+	def addMDFilter( self, filter ):
+		if filter != None and self.checkFilterType( filter ) == "2_2":
 			if self.MDFilter == None:
 				self.MDFilter = []
-			self.MDFilter.append(filter)
-	def removeMDFilter(self, filter):
-		if filter != None and self.checkFilterType(filter) == "2_2":
-			ind = self.MDFilter.index(filter)
+			self.MDFilter.append( filter )
+
+	def removeMDFilter( self, filter ):
+		if filter != None and self.checkFilterType( filter ) == "2_2":
+			ind = self.MDFilter.index( filter )
 			if ind != -1:
-				self.MDFilter.pop(ind)
-				if len(self.MDFilter) == 0:
+				self.MDFilter.pop( ind )
+				if len( self.MDFilter ) == 0:
 					self.MDFilter = None
